4aa486a76edb8dde8912a6f9f580d0275691309c
Jacob Evans <jakegod14@gmail.com>
2019-01-21 09:41:37 +0100

    Finish rough draft of slides

--- presentation/presentation/index.js
+++ presentation/presentation/index.js
@@ -13,6 +13,9 @@ import {
   CodePane
 } from 'spectacle'
 import WhatIsGrpcImage from '../assets/grpc.svg'
+import GeneralArch from '../assets/general.svg'
+import Build from '../assets/build.svg'
+import ChristmasArch from '../assets/arch.svg'
 
 // move to other files
 const protobufExample = `// The greeter service definition.
@@ -84,7 +87,7 @@ class Presentation extends Component {
       <Deck transition={['zoom', 'slide']} transitionDuration={500} theme={theme}>
         <Slide transition={['zoom']} bgColor='primary'>
           <Heading size={1} fit caps lineHeight={1} textColor='secondary'>
-            GRPC-Web: A primer
+            gRPC-Web: A primer
           </Heading>
           <Text margin='10px 0 0' textColor='tertiary' size={1} fit bold>
             By Cornelio Hopmann and Jacob Evans
@@ -108,15 +111,15 @@ class Presentation extends Component {
         <Slide transition={['fade']} bgColor='primary' textColor='tertiary'>
           <Heading size={4} textColor='secondary' caps>What will be covered</Heading>
           <List>
-            <ListItem>What is grpc?</ListItem>
-            <ListItem>What is grpc-web?</ListItem>
-            <ListItem>Closer look at grpc-web</ListItem>
-            <ListItem>Example grpc-web project</ListItem>
-            <ListItem>Grpc-web</ListItem>
+            <ListItem>What is gRPC?</ListItem>
+            <ListItem>What is gRPC-web?</ListItem>
+            <ListItem>Closer look at gRPC-web</ListItem>
+            <ListItem>Example gRPC-web project</ListItem>
+            <ListItem>gRPC-web</ListItem>
           </List>
         </Slide>
         <Slide transition={['fade']} bgColor='tertiary' textColor='primary'>
-          <Heading size={5} textColor='secondary' caps>What is grpc?</Heading>
+          <Heading size={5} textColor='secondary' caps>What is gRPC?</Heading>
           <BlockQuote>
             <Quote textSize={24}>In gRPC a client application can directly call methods on a server application on a different machine as if it was a local object, making it easier for you to create distributed applications and services. As in many RPC systems, gRPC is based around the idea of defining a service, specifying the methods that can be called remotely with their parameters and return types. On the server side, the server implements this interface and runs a gRPC server to handle client calls. On the client side, the client has a stub (referred to as just a client in some languages) that provides the same methods as the server.</Quote>
           </BlockQuote>
@@ -133,12 +136,12 @@ class Presentation extends Component {
         </Slide>
         <Slide transition={['fade']} bgColor='tertiary'>
           <Heading size={1} fit caps lineHeight={1} textColor='secondary'>
-            GRPC Basics
+            gRPC Basics
           </Heading>
         </Slide>
         <Slide transition={['fade']} bgColor='primary' textColor='tertiary'>
           <Heading size={6} textColor='secondary' caps>Services</Heading>
-          <Text textAlign={'left'} padding={'10px 0px'} textSize={24} textColor='secondary'>GRPC is based around the idea of defining a service, specifying the methods that can be called remotely with their parameters and return types.</Text>
+          <Text textAlign={'left'} padding={'10px 0px'} textSize={24} textColor='secondary'>gRPC is based around the idea of defining a service, specifying the methods that can be called remotely with their parameters and return types.</Text>
           <CodePane
             lang='go'
             source={grpcServiceExample}
@@ -171,12 +174,12 @@ class Presentation extends Component {
           <Heading size={6} textColor='secondary' caps>Using these methods</Heading>
           <Text textAlign={'left'} padding={'10px 0px'} textSize={24} textColor='secondary'>Starting from a service definition in a .proto file, gRPC provides protocol buffer compiler plugins that generate client- and server-side code. gRPC users typically call these APIs on the client side and implement the corresponding API on the server side.</Text>
           <Text textAlign={'left'} padding={'10px 0px'} textSize={24} textColor='secondary'>The server waits for incoming rpc calls implements a service then performs operations and sends messages back to the client.</Text>
-          <Text textAlign={'left'} padding={'10px 0px'} textSize={24} textColor='secondary'>The client sends rpc calls to the grpc server and then waits for the messages back from the grpc server.</Text>
+          <Text textAlign={'left'} padding={'10px 0px'} textSize={24} textColor='secondary'>The client sends rpc calls to the gRPC server and then waits for the messages back from the gRPC server.</Text>
         </Slide>
         <Slide transition={['fade']} bgColor='tertiary' textColor='primary'>
           <Heading size={6} textColor='secondary' caps>Other important concepts</Heading>
           <Text textAlign={'left'} padding={'10px 0px'} textSize={24} x>Deadlines/Timeouts</Text>
-          <Text textAlign={'left'} padding={'5px 0px'} textSize={18} textColor='primary'>GRPC allows clients to specify how long they are willing to wait for an RPC to complete before the RPC is terminated with the error DEADLINE_EXCEEDED. On the server side, the server can query to see if a particular RPC has timed out, or how much time is left to complete the RPC.</Text>
+          <Text textAlign={'left'} padding={'5px 0px'} textSize={18} textColor='primary'>gRPC allows clients to specify how long they are willing to wait for an RPC to complete before the RPC is terminated with the error DEADLINE_EXCEEDED. On the server side, the server can query to see if a particular RPC has timed out, or how much time is left to complete the RPC.</Text>
           <Text textAlign={'left'} padding={'10px 0px'} textSize={24} textColor='secondary'>RPC Termination</Text>
           <Text textAlign={'left'} padding={'5px 0px'} textSize={18} textColor='primary'>In gRPC, both the client and server make independent and local determinations of the success of the call, and their conclusions may not match. This means that, for example, you could have an RPC that finishes successfully on the server side (“I have sent all my responses!”) but fails on the client side (“The responses arrived after my deadline!”). It’s also possible for a server to decide to complete before a client has sent all its requests.</Text>
           <Text textAlign={'left'} padding={'10px 0px'} textSize={24} textColor='secondary'>Cancelling RPCs</Text>
@@ -186,7 +189,7 @@ class Presentation extends Component {
         </Slide>
         <Slide transition={['fade']} bgColor='primary'>
           <Heading size={1} fit caps lineHeight={1} textColor='secondary'>
-            GRPC-Web
+            gRPC-Web
           </Heading>
         </Slide>
         <Slide transition={['fade']} bgColor='tertiary' textColor='secondary'>
@@ -194,11 +197,11 @@ class Presentation extends Component {
           <BlockQuote>
             <Text textAlign={'left'} textSize={32} padding={'5px 0px'} textColor='primary'>gRPC web is a library that allows browsers to run gRPC</Text>
             <Text textAlign={'left'} textSize={32} padding={'5px 0px'} textColor='primary'>It providers a Code Generator Plugin for protoc that outputs browser ready code.</Text>
-            <Text textAlign={'left'} textSize={32} padding={'5px 0px'} textColor='primary'>Check it out on github.com/grpc/grpc-web</Text>
+            <Text textAlign={'left'} textSize={32} padding={'5px 0px'} textColor='primary'>Check it out on github.com/gRPC/gRPC-web</Text>
           </BlockQuote>
         </Slide>
         <Slide transition={['fade']} bgColor='primary' textColor='secondary'>
-          <Heading size={5} textColor='secondary' caps>What does it give?</Heading>
+          <Heading size={5} textColor='secondary' caps>What does it generate?</Heading>
           <BlockQuote>
             <Text textAlign={'left'} textSize={32} padding={'5px 0px'} textColor='secondary'>{`{service}/_pub.js}`} The compiled client code.</Text>
             <Text textAlign={'left'} textSize={32} padding={'5px 0px'} textColor='secondary'>{`{service}/web_pub.js}`} The compiled requests code.</Text>
@@ -206,22 +209,64 @@ class Presentation extends Component {
         </Slide>
         <Slide transition={['fade']} bgColor='tertiary' textColor='secondary'>
           <Heading size={5} textColor='secondary' caps>Architecture</Heading>
-          {'Core put image here'}
-          <Image padding={'20px 0px'} src={WhatIsGrpcImage} height={300} />
+          <Image padding={'20px 0px'} src={GeneralArch} height={400} />
         </Slide>
         <Slide transition={['fade']} bgColor='primary' textColor='secondary'>
-          <Heading size={5} textColor='secondary' caps>Using it in React</Heading>
-          <Text textAlign={'left'} textSize={32} padding={'5px 0px'} textColor='secondary'>Modify your build step babel build step to support commonjs modules.</Text>
-          <Text textAlign={'left'} textSize={32} padding={'5px 0px'} textColor='secondary'>Use normal your React webpack build.</Text>
+          <Heading size={5} textColor='secondary' caps>Using gRPC with React</Heading>
+          <Text textAlign={'left'} textSize={32} padding={'5px 0px'} textColor='secondary'>Make sure your webpack build supports commonjs modules.</Text>
+          <Image padding={'20px 0px'} src={Build} height={200} />
         </Slide>
         <Slide transition={['fade']} bgColor='tertiary' textColor='secondary'>
+          <Heading size={5} textColor='secondary' caps>Warning</Heading>
+          <Text textAlign={'left'} textSize={32} padding={'5px 0px 10px 0px'} textColor='primary'>gRPC needs access HTTP/2 trailers, which is not supported in browsers today. This means some form of proxy is required.</Text>
+          <Text textAlign={'left'} textSize={32} padding={'5px 0px'} textColor='secondary'>More Info Here</Text>
+          <Text textAlign={'left'} textSize={24} padding={'2px 0px'} textColor='primary'>github.com/grpc/grpc/blob/master/doc/PROTOCOL-WEB.md</Text>
+          <Text textAlign={'left'} textSize={24} padding={'2px 0px'} textColor='primary'>github.com/grpc/grpc-web/issues/347</Text>
+          <Text textAlign={'left'} textSize={24} padding={'2px 0px'} textColor='primary'>github.com/grpc/grpc/issues/2786</Text>
+        </Slide>
+        <Slide transition={['fade']} bgColor='primary' textColor='secondary'>
           <Heading size={5} textColor='secondary' caps>Example Project</Heading>
-          <Text textAlign={'left'} textSize={32} padding={'5px 0px'} textColor='secondary'>Checkout the project here</Text>
-          <Text textAlign={'left'} textSize={32} padding={'5px 0px'} textColor='primary'>github.com/JacobTheEvans/christmas-cards</Text>
+          <Text textAlign={'left'} textSize={36} padding={'5px 0px'} textColor='secondary'>github.com/JacobTheEvans/christmas-cards</Text>
+          <Image padding={'20px 0px'} src={ChristmasArch} height={370} />
+        </Slide>
+        <Slide transition={['fade']} bgColor='tertiary'>
+          <Heading size={1} fit caps lineHeight={1} textColor='secondary'>
+            Group Discussion
+          </Heading>
+        </Slide>
+        <Slide transition={['fade']} bgColor='primary' textColor='secondary'>
+          <Heading size={5} textColor='secondary' caps>RPC Use</Heading>
+          <Text textAlign={'left'} textSize={32} padding={'5px 0px'} textColor='secondary'>Who has used RPCs before?</Text>
+          <Text textAlign={'left'} textSize={32} padding={'5px 0px'} textColor='secondary'>What RPC frameworks did you use?</Text>
+          <Text textAlign={'left'} textSize={32} padding={'5px 0px'} textColor='secondary'>How do you feel about RPC as a solution?</Text>
+          <Text textAlign={'left'} textSize={32} padding={'5px 0px'} textColor='secondary'>What contexts are you using it in?</Text>
         </Slide>
         <Slide transition={['fade']} bgColor='tertiary' textColor='secondary'>
-          <Heading size={5} textColor='secondary' caps>Discussion</Heading>
-          <Text textAlign={'left'} textSize={32} padding={'5px 0px'} textColor='secondary'>Write questions</Text>
+          <Heading size={5} textColor='secondary' caps>gRPC Use</Heading>
+          <Text textAlign={'left'} textSize={32} padding={'5px 0px'} textColor='primary'>Who is using gRPC?</Text>
+          <Text textAlign={'left'} textSize={32} padding={'5px 0px'} textColor='primary'>What contexts are you using it in?</Text>
+          <Text textAlign={'left'} textSize={32} padding={'5px 0px'} textColor='primary'>Who is using gRPC-web?</Text>
+          <Text textAlign={'left'} textSize={32} padding={'5px 0px'} textColor='primary'>What contexts are you using it in?</Text>
+        </Slide>
+        <Slide transition={['fade']} bgColor='primary' textColor='secondary'>
+          <Heading size={5} textColor='secondary' caps>Web Interface Solution</Heading>
+          <Text textAlign={'left'} textSize={32} padding={'5px 0px'} textColor='secondary'>What do you think of using gRPC-web as a frontend solution?</Text>
+          <Text textAlign={'left'} textSize={32} padding={'5px 0px'} textColor='secondary'>What do you think of using gRPC as a backend solution?</Text>
+          <Text textAlign={'left'} textSize={32} padding={'5px 0px'} textColor='secondary'>What do you think of using gRPC in combination with GraphQL?</Text>
+        </Slide>
+        <Slide transition={['fade']} bgColor='tertiary'>
+          <Heading size={1} fit caps lineHeight={1} textColor='secondary'>
+            Free Discussion
+          </Heading>
+        </Slide>
+        <Slide transition={['fade']} bgColor='primary' textColor='secondary'>
+          <Heading size={5} textColor='secondary' caps>Thanks for your time</Heading>
+          <Text textAlign={'left'} padding={'20px 0px'} textSize={30} textColor='secondary'>
+          Jacob Evans (@jacobtheevans)
+          </Text>
+          <Text textAlign={'left'} padding={'20px 0px'} textSize={30} textColor='secondary'>
+          Cornelio Hopmann (@chopmann)
+          </Text>
         </Slide>
       </Deck>
     )
